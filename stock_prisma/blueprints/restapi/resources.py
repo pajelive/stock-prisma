4d679274c5748fc060ed318a8c651ea6f84cbc0f
pajelive <soaresromulo444@gmail.com>
2026-05-20 16:30:35 -0300

    Criação de Service com algumas regras para api comunicar com Models

--- stock_prisma/blueprints/restapi/resources.py
+++ stock_prisma/blueprints/restapi/resources.py
@@ -1,9 +1,9 @@
 from flask_restful import Resource
 from flask import request
+from stock_prisma.services.MovimentacaoService import MovimentacaoService
 
 from stock_prisma.models import (
-    Compartimento,
-    Movimentacao
+    Compartimento
 )
 
 from stock_prisma.ext.database import db
@@ -35,40 +35,16 @@ class MovimentacaoResource(Resource):
 
         dados = request.json
 
-        usuario_uid = dados.get("usuario_uid")
-        compartimento_id = dados.get("compartimento_id")
-        peso_atual = dados.get("peso_atual")
-        tipo_nome = dados.get("tipo_movimentacao")
-        origem = dados.get("origem")
+        try:
+            mov = MovimentacaoService.registrar(dados)
 
-        usuario = Usuario.query.filter_by(uid_rfid=usuario_uid).first()
+            return {
+                "msg": "Movimentação registrada",
+                "id": mov.id
+            }, 201
 
-        if not usuario:
-            return {"erro": "Usuário não encontrado"}, 404
+        except ValueError as e:
+            return {"erro": str(e)}, 400
 
-        compartimento = Compartimento.query.get(compartimento_id)
-
-        if not compartimento:
-            return {"erro": "Compartimento não encontrado"}, 404
-
-        tipo = TipoMovimentacao.query.filter_by(nome=tipo_nome).first()
-
-        if not tipo:
-            return {"erro": "Tipo movimentação inválido"}, 404
-
-        # atualiza peso do compartimento
-        compartimento.peso_atual = peso_atual
-
-        movimentacao = Movimentacao(
-            usuario_id=usuario.id,
-            compartimento_id=compartimento.id,
-            tipo_movimentacao_id=tipo.id,
-            quantidade=1,
-            origem_leitura=origem,
-            observacao="Movimentação automática"
-        )
-
-        db.session.add(movimentacao)
-        db.session.commit()
-
-        return {"msg": "Movimentação registrada com sucesso"}, 201    
+        except Exception as e:
+            return {"erro": "erro interno"}, 500
---
+++ stock_prisma/services/MovimentacaoService.py
@@ -0,0 +1,50 @@
+from stock_prisma.ext.database import db
+
+from stock_prisma.models import (Usuario,Compartimento,TipoMovimentacao,Movimentacao,Ferramenta)
+
+class MovimentacaoService:
+
+    @staticmethod
+    def registrar_movimentacao(data):
+
+        usuario = Usuario.query.filter_by(
+            uid_rfid=data["usuario_uid"]
+        ).first()
+
+        if not usuario:
+            raise Exception("Usuário não encontrado")
+
+        compartimento = None
+        if data.get("compartimento_id"):
+            compartimento = Compartimento.query.get(data["compartimento_id"])
+
+        ferramenta = None
+        if data.get("ferramenta_id"):
+            ferramenta = Ferramenta.query.get(data["ferramenta_id"])
+
+        tipo = None
+        if data.get("tipo_movimentacao"):
+            tipo = TipoMovimentacao.query.filter_by(
+                nome=data["tipo_movimentacao"]
+            ).first()
+
+        if compartimento:
+            compartimento.peso_atual = data.get(
+                "peso_atual",
+                compartimento.peso_atual
+            )
+
+        mov = Movimentacao(
+            usuario_id=usuario.id,
+            compartimento_id=compartimento.id if compartimento else None,
+            ferramenta_id=ferramenta.id if ferramenta else None,
+            tipo_movimentacao_id=tipo.id if tipo else None,
+            quantidade=data.get("quantidade", 1),
+            origem_leitura=data.get("origem", "API"),
+            observacao=data.get("observacao")
+        )
+
+        db.session.add(mov)
+        db.session.commit()
+
+        return mov
