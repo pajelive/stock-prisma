627d1bc5cbd8508c4fb47227deef9bd6b7b57175
pajelive <soaresromulo444@gmail.com>
2026-05-21 18:14:17 -0300

    teste deply

--- stock_prisma/app.py
+++ stock_prisma/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# Blueprints da API
+from stock_prisma.blueprints.restapi import init_app as restapi_init
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
-    print("APP INICIOU OK")
-
+    # rota de saúde (obrigatória na Vercel)
     @app.get("/")
     def home():
-        return {"status": "ok"}
+        return jsonify({"status": "ok"})
+
+    # registra API
+    restapi_init(app)
+
+    print("[BOOT] API carregada:", app.url_map)
 
     return app
