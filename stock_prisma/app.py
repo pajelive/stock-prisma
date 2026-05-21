963a8bd1e2e895fcb220637f93327efe47c3de8b
pajelive <soaresromulo444@gmail.com>
2026-05-21 18:42:44 -0300

    teste deply

--- stock_prisma/app.py
+++ stock_prisma/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-# Blueprints da API
 from stock_prisma.blueprints.restapi import init_app as restapi_init
+from stock_prisma.ext.database import init_app as db_init   # 👈 ADD ISSO
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # rota de saúde (obrigatória na Vercel)
+    # rota de saúde
     @app.get("/")
     def home():
         return jsonify({"status": "ok"})
+
+    # 🔥 ISSO AQUI FALTAVA
+    db_init(app)
 
     # registra API
     restapi_init(app)
