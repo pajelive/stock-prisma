72af27e61e9e61bdcff3d07053e40d850f47cfc8
pajelive <soaresromulo444@gmail.com>
2026-05-21 19:41:36 -0300

    teste deply marco 1

--- stock_prisma/ext/database.py
+++ stock_prisma/ext/database.py
@@ -5,15 +5,18 @@ from sqlalchemy.pool import NullPool
 
 Base = declarative_base()
 
-DATABASE_URL = os.environ.get("POSTGRES_PRISMA_URL")
+DATABASE_URL = os.getenv("POSTGRES_PRISMA_URL")
+
+if not DATABASE_URL:
+    raise Exception("POSTGRES_PRISMA_URL não está definida na Vercel")
 
 engine = create_engine(
     DATABASE_URL,
-    poolclass=NullPool,   # importante pra serverless
-    pool_pre_ping=True
+    poolclass=NullPool,
+    pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(bind=engine)
 
 def get_session():
     return SessionLocal()
