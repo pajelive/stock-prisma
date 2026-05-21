8e572d3b36c29f51b10f62a6bb9499783dddb7f6
pajelive <soaresromulo444@gmail.com>
2026-05-21 17:04:47 -0300

    deploy produção

--- stock_prisma/app.py
+++ stock_prisma/app.py
@@ -1,10 +1,13 @@
 from stock_prisma.ext import configuration
 from flask import Flask, render_template
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     configuration.init_app(app)
     configuration.load_extensions(app)
+    CORS(app)
+
     return app
 
 
