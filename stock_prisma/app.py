8c87e1a45058a7ce78070374885b3c4d2cc99ffb
pajelive <soaresromulo444@gmail.com>
2026-05-21 18:07:31 -0300

    teste deply

--- stock_prisma/app.py
+++ stock_prisma/app.py
@@ -1,24 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
-
-from stock_prisma.ext.database import init_app as db_init
-from stock_prisma.blueprints.restapi import init_app as restapi_init
-from stock_prisma.blueprints.views import init_app as views_init
-
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
 
-    try:
-        db_init(app)
-        restapi_init(app)
-        views_init(app)
-        print("[BOOT] app iniciado com sucesso")
-        print(app.url_map)
+    print("APP INICIOU OK")
 
-    except Exception as e:
-        print("[FATAL ERROR]", str(e))
-        raise
+    @app.get("/")
+    def home():
+        return {"status": "ok"}
 
     return app
