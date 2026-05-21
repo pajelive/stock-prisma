f14ff3cd8c07a6a1ccc09e68b4331f61a8ef5efa
pajelive <soaresromulo444@gmail.com>
2026-05-21 19:24:27 -0300

    teste deply marco 1

--- stock_prisma/ext/database.py
+++ stock_prisma/ext/database.py
@@ -4,12 +4,6 @@ from sqlalchemy.pool import NullPool
 db = SQLAlchemy()
 
 def init_app(app):
-    uri = app.config.get("SQLALCHEMY_DATABASE_URI")
-
-    if not uri:
-        raise RuntimeError("DATABASE URI not configured")
-
-    # configuração correta do SQLAlchemy no Flask-SQLAlchemy
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
         "poolclass": NullPool,
         "pool_pre_ping": True
