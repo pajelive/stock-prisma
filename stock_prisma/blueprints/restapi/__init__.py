cd7f77f4c49db488676a6a24cf07fa226183170f
pajelive <soaresromulo444@gmail.com>
2026-05-21 18:15:47 -0300

    teste deply

--- stock_prisma/blueprints/restapi/__init__.py
+++ stock_prisma/blueprints/restapi/__init__.py
@@ -1,17 +1,20 @@
 from flask import Blueprint
 from flask_restful import Api
-from .resources import (CompartimentoResource,MovimentacaoResource, TipoRFIDResource)
 
-bp = Blueprint("restapi", __name__,url_prefix="/api/v1")
+from .resources import (
+    CompartimentoResource,
+    MovimentacaoResource,
+    TipoRFIDResource
+)
+
+bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
 
 api = Api(bp)
 
-api.add_resource(CompartimentoResource,"/compartimentos")
+api.add_resource(CompartimentoResource, "/compartimentos")
+api.add_resource(MovimentacaoResource, "/movimentacoes")
+api.add_resource(TipoRFIDResource, "/rfid/<string:uid>")
 
-api.add_resource(MovimentacaoResource,"/movimentacoes")
 
-api.add_resource(TipoRFIDResource,"/rfid/<string:uid>")
-
-# conecta blueprint no flask
 def init_app(app):
     app.register_blueprint(bp)
