81e1df5e4b6451cb34cd6a6ef27fd98fe14cd3a7
pajelive <soaresromulo444@gmail.com>
2026-05-21 18:03:49 -0300

    teste deply

--- stock_prisma/app.py
+++ stock_prisma/app.py
@@ -8,16 +8,17 @@ from stock_prisma.blueprints.views import init_app as views_init
 
 def create_app():
     app = Flask(__name__)
-
     CORS(app)
 
-    # DB
-    db_init(app)
+    try:
+        db_init(app)
+        restapi_init(app)
+        views_init(app)
+        print("[BOOT] app iniciado com sucesso")
+        print(app.url_map)
 
-    # ROTAS (SEM Dynaconf)
-    restapi_init(app)
-    views_init(app)
-
-    print("[BOOT] Rotas registradas:", app.url_map)
+    except Exception as e:
+        print("[FATAL ERROR]", str(e))
+        raise
 
     return app
