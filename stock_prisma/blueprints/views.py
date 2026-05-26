22c9ffe75bc296b8a7d772fa771e44d29f63f45e
paje.live <soaresromulo444@gmail.com>
2026-05-25 21:17:05 -0300

    criando as rotas principais da aplicacao

--- stock_prisma/blueprints/views.py
+++ stock_prisma/blueprints/views.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template
-#import database
+from stock_prisma.models import Movimentacao, Usuario, Ferramenta
+from stock_prisma.ext.database import db
 
 def init_app(app):
+
     @app.route('/')
     def index():
-        products = ['teste1','teste2','teste3']
-        return render_template("index.html", products=products)
+        return render_template("index.html")
+
+    @app.route('/historico')
+    def historico():
+        movimentacoes = db.session.query(
+            Movimentacao
+        ).order_by(
+            Movimentacao.data_hora.desc()
+        ).limit(100).all()
+
+        return render_template(
+            "historico.html",
+            movimentacoes=movimentacoes
+        )
+
+    @app.route('/ferramentas')
+    def ferramentas():
+        ferramentas = Ferramenta.query.filter_by(ativo=True).all()
+        return render_template(
+            "ferramentas.html",
+            ferramentas=ferramentas
+        )
