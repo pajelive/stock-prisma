fde153b98064a1a15665d4859a049de2bed44ede
paje.live <soaresromulo444@gmail.com>
2026-05-27 20:08:37 -0300

    criando components

---
+++ stock_prisma_interface/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <nav>
+            <div>
+                <h1></h1>
+                <ul>
+                </ul>
+            </div>
+        </nav>
+    )
+}
---
+++ stock_prisma_interface/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <footer>
+            <div>
+                <h2></h2>
+                <p>
+                </p>
+            </div>
+        </footer>
+    )
+}
