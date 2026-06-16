b76d1a7bc94183dfbd113243904b15d5dd0274c4
paje.live <soaresromulo444@gmail.com>
2026-06-16 19:51:11 -0300

    Criando função de return no React para o html de login

--- stock_prisma_interface/src/components/TelaLogin.jsx
+++ stock_prisma_interface/src/components/TelaLogin.jsx
@@ -1,18 +1,13 @@
+import public from '../../public/logo_nav.png'
+
 export default function TelaLogin() {
     return (
         <div className="container">
             <div className="left-panel">
-                <svg className="logo-svg" viewBox="0 0 200 190" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <polygon points="100,8 192,168 8,168" fill="none" stroke="#29A8FF" stroke-width="9"
-                             stroke-linejoin="round"/>
-                    <polygon points="100,8 54,95 100,95" fill="#0A0A55"/>
-                    <polygon points="100,8 146,95 100,95" fill="#1189D0"/>
-                    <polygon points="54,95 100,168 146,95" fill="#0A0A55" opacity="0.75"/>
-                </svg>
                 <h1>STOCK <span>PRISMA</span></h1>
                 <p className="slogan">A descoberta é o que nos move.</p>
             </div>
-
+            <img src="logo_nav.png" alt=""/>
             <div className="right-panel">
                 <div className="login-card">
                     <h2>Login</h2>
@@ -26,9 +21,9 @@ export default function TelaLogin() {
                         <div className="remember-forgot">
                             <input type="checkbox"/>
                         </div>
-                <button type="submit" className="btn-login"> Login</button>
-                <a href="cadastro.html" className="btn-register"> Cadastrar</a>
-            </form>
+                        <button type="submit" className="btn-login"> Login</button>
+                        <a href="cadastro.html" className="btn-register"> Cadastrar</a>
+                    </form>
         </div>
     </div>
 
