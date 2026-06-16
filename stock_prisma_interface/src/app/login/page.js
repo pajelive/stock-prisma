cdb2c2297cec774650767924b4cf4083869618fa
paje.live <soaresromulo444@gmail.com>
2026-06-16 19:25:56 -0300

    Adicionado renderização do html de login

--- stock_prisma_interface/src/app/login/page.js
+++ stock_prisma_interface/src/app/login/page.js
@@ -1,5 +1,9 @@
-export default function Home() {
+import TelaLogin from "@/components/TelaLogin";
+
+export default function Login() {
     return (
-         <h1>Home</h1>
+        <div className="p-6 bg-gray-50 min-h-screen">
+            <TelaLogin titulo="Histórico" dados={movimentacoes} />
+        </div>
     )
 }
