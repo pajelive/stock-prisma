1b746a4b01339a94d10a7945b86c9a048eb09c0a
paje.live <soaresromulo444@gmail.com>
2026-06-16 19:32:08 -0300

    Adicionado renderização do html de login

--- stock_prisma_interface/src/app/login/page.js
+++ stock_prisma_interface/src/app/login/page.js
@@ -3,7 +3,7 @@ import TelaLogin from '../../components/TelaLogin'
 export default function Login() {
     return (
         <div className="p-6 bg-gray-50 min-h-screen">
-            <TelaLogin titulo="Histórico" dados={movimentacoes} />
+            <TelaLogin titulo="Login"/>
         </div>
     )
 }
