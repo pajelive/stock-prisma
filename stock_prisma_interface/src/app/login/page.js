a1a1dfa88248e47933edfad62eb684639ede0d7b
paje.live <soaresromulo444@gmail.com>
2026-06-17 11:26:54 -0300

    Centralizando componente Login

--- stock_prisma_interface/src/app/login/page.js
+++ stock_prisma_interface/src/app/login/page.js
@@ -1,5 +1,5 @@
 import TelaLogin from '../../components/TelaLogin'
-import styles from './page.module.scss'
+import styles from './page.module.css'
 
 export default function Login() {
     return (
