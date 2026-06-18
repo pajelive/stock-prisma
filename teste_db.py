2c3e2de87c94b7ef1b3787c2dd4571d904f68d6a
paje.live <soaresromulo444@gmail.com>
2026-06-18 11:15:08 -0300

    Login dando errado

--- teste_db.py
+++ teste_db.py
@@ -1,60 +1,17 @@
-from stock_prisma.app import create_app
-from stock_prisma.ext.database import db
+from sqlalchemy import create_engine, text
+from werkzeug.security import generate_password_hash
 
-from stock_prisma.models import Ferramenta, Usuario, Compartimento, TipoMovimentacao, Movimentacao
+# 🔥 SUA CONEXÃO DO BANCO AQUI
+DATABASE_URL = "COLE_AQUI_SUA_URL"
 
-from datetime import datetime
+engine = create_engine(DATABASE_URL)
 
-app = create_app()
+nova_senha = generate_password_hash("123456789")
 
-with app.app_context():
-
-    # -------------------------
-    # 1. cria ferramenta
-    # -------------------------
-    ferramenta = Ferramenta(
-        nome="Parafusadeira Bosch",
-        categoria="Elétrica",
-        descricao="Ferramenta elétrica de montagem",
-        uid_rfid="RFID_FERR_001",
-        status="DISPONIVEL",
-        ativo=True
+with engine.begin() as conn:
+    conn.execute(
+        text("UPDATE usuario SET senha_hash = :senha"),
+        {"senha": nova_senha}
     )
 
-    db.session.add(ferramenta)
-    db.session.commit()
-
-    print("[OK] Ferramenta criada")
-
-    # -------------------------
-    # 2. busca dependências
-    # -------------------------
-    usuario = Usuario.query.first()
-    compartimento = Compartimento.query.first()
-    tipo = TipoMovimentacao.query.first()
-
-    if not usuario or not compartimento or not tipo:
-        print("❌ Dados base não encontrados")
-        exit()
-
-    # -------------------------
-    # 3. cria movimentação com ferramenta
-    # -------------------------
-    mov = Movimentacao(
-        usuario_id=usuario.id,
-        compartimento_id=compartimento.id,
-        ferramenta_id=ferramenta.id,
-        tipo_movimentacao_id=tipo.id,
-
-        quantidade=1,
-        data_hora=datetime.now(),
-        origem_leitura="RFID",
-        observacao="Teste ferramenta"
-    )
-
-    db.session.add(mov)
-    db.session.commit()
-
-    print("[OK] Movimentação com ferramenta criada")
-
-    print("\n🔥 Teste finalizado com sucesso!")
+print("✔ Senhas resetadas com sucesso")
