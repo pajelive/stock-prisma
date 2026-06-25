01088b3dd3434a5177f8ceb440702a7598db4dc5
paje.live <soaresromulo444@gmail.com>
2026-06-25 12:16:17 -0300

    Tentando deixar video responsivo

--- stock_prisma_interface/src/app/page.js
+++ stock_prisma_interface/src/app/page.js
@@ -1,13 +1,12 @@
 export default function Home() {
     return (
-        <main className="relative flex h-screen w-screen items-center justify-center overflow-hidden bg-[#F1F1F1]">
+        <main className="relative flex h-screen w-screen items-center justify-center overflow-hidden bg-[#E8E8E8]">
             <video
                 autoPlay
                 loop
                 muted
                 playsInline
-                className="absolute inset-0 h-full w-full object-contain md:object-cover z-0 pointer-events-none"
-            >
+                className="absolute top-0 left-0 h-[60%] w-full object-contain md:h-full md:object-cover z-0 pointer-events-none md:object-center">
                 <source src="/logo-loop.mp4" type="video/mp4" />
                 Seu navegador não suporta a tag de vídeo.
             </video>
