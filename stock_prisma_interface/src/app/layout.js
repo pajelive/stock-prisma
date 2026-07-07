ab5f027a9c405ab8d1fe55fd3fa5b198cfc2b590
paje.live <soaresromulo444@gmail.com>
2026-07-07 15:33:31 -0300

    Alteradno api pra exibir o nome do insumo

--- stock_prisma_interface/src/app/layout.js
+++ stock_prisma_interface/src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="pt-BR"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${jakarta.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <AuthProvider>
